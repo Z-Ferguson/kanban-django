031255897c3a52e27c013936463bfb0eafed837e
Z-Ferguson <zfergusonn@gmail.com>
2016-12-01 17:12:03 -0500

    added user form and model to files

--- kanban_app/models.py
+++ kanban_app/models.py
@@ -1,7 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
 class Tasks(models.Model):
     title = models.CharField(max_length=255)
     status = models.CharField(max_length=255)
     priority = models.CharField(max_length=255)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
