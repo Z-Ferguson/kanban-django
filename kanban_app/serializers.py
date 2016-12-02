c82dca614215ef47deb96fba2b539bd32f2a81e9
Z-Ferguson <zfergusonn@gmail.com>
2016-12-02 15:43:58 -0500

    api is unbroken again, added fields in serializers for input

--- kanban_app/serializers.py
+++ kanban_app/serializers.py
@@ -4,16 +4,21 @@ from django.contrib.auth.models import User
 
 
 class TaskSerializer(serializers.Serializer):
-    
+
+    title = serializers.CharField(max_length=250)
     priority = serializers.ChoiceField(choices=Tasks.priority_choices)
     status = serializers.ChoiceField(choices=Tasks.status_choices)
+    description = serializers.CharField(max_length=250)
+
+    def create(self, validated_data):
+        return Tasks(**validated_data)
 
     class Meta:
         model = Tasks
         fields = ('url', 'id', 'title', 'status', 'priority', 'description', 'owner')
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
-    full_name = serializers.CharField(source='get_full_name', read_only=True)
+    full_name = serializers.CharField(source='get_full_name')
 
     class Meta:
         model = User
