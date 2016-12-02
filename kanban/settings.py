3388f47ad6e9fc39bb57e0368425b754c54a4653
John D. Bower <jdbower0525@gmail.com>
2016-12-02 11:44:47 -0500

    got js working

--- kanban/settings.py
+++ kanban/settings.py
@@ -63,7 +63,6 @@ TEMPLATES = [
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
          os.path.join(BASE_DIR, 'cloneapp/templates'),
-         os.path.join(BASE_DIR, 'cloneapp/templates/registration'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -120,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
---
+++ kanban_app/static/kanban_app/js/main.js
@@ -0,0 +1,14 @@
+console.log('you loaded js right')
+// function taskList(){
+//     console.log('you loaded js right')
+//     // var $orderedlist = $("<ol>")
+//     //     $.ajax("http://127.0.0.1:8000/api").done(function(results){
+//     //         var tasks = results.results
+//     //         for(var i = 0; i < tasks.length; i++){
+//     //             $stuff.html($stuff.html()+ tasks[i]['title'] + "<br>")
+//     //             $("#location").append($orderedlist),
+//     // $("#location").append("NONONONONONONONO")
+// //             }
+// //         })
+// }
+// taskList()
