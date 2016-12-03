2fca61c7fb21023a756cac9446942d56b3e38dff
John D. Bower <jdbower0525@gmail.com>
2016-12-03 12:38:12 -0500

    got authorizations working

--- kanban_app/static/kanban_app/js/main.js
+++ kanban_app/static/kanban_app/js/main.js
@@ -1,15 +1,15 @@
 
 
 function taskPost(){
-  var postdata = {'title': 'TEST', 'status': 'HIGH', 'priority': 'LOW'}
-  jQuery.ajax({url:'http://127.0.0.1:8000/api/', data:postdata, type:'POST'
+  var postdata = {'title': 'TEST', 'status': 'HIGH', 'priority': 'LOW', 'description': 'new description'}
+  jQuery.ajax({url:'http://127.0.0.1:8000/api/task', data:postdata, type:'POST'
     }).done(function(){})
 }
 
 
 function taskList(){
     var $orderedlist = $("<ol>")
-        $.ajax("http://127.0.0.1:8000/api").done(function(results){
+        $.ajax("http://127.0.0.1:8000/api/task").done(function(results){
             var tasks = results.results
             for(var i = 0; i < tasks.length; i++){
                 $orderedlist.html($orderedlist.html()+ tasks[i]['title'] + ": "),
