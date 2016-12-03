2bdb03da389f9271eeb261910ceea4567d8f4e29
John D. Bower <jdbower0525@gmail.com>
2016-12-03 15:19:41 -0500

    html changes

--- kanban_app/static/kanban_app/js/main.js
+++ kanban_app/static/kanban_app/js/main.js
@@ -1,37 +1,85 @@
-// 
+function getCookie(name) {
+   var cookieValue = null;
+   if (document.cookie && document.cookie !== '') {
+       var cookies = document.cookie.split(';');
+       for (var i = 0; i < cookies.length; i++) {
+           var cookie = jQuery.trim(cookies[i]);
+           if (cookie.substring(0, name.length + 1) === (name + '=')) {
+               cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+               break;
+           }
+       }
+   }
+   return cookieValue;
+}
+
+
+var csrftoken = getCookie('csrftoken');
+
+function csrfSafeMethod(method) {
+   return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+
+
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrftoken);
+        }
+    }
+});
+
+
+
+//POST
+// var $addTask = $('#addTask');
+
+
+// function addTask(){
+//   console.log('You added a fresh task');
 //
-// function taskPost(){
-//   var postdata = {'title': 'TEST', 'status': 'HIGH', 'priority': 'LOW', 'description': 'new description'}
-//   jQuery.ajax({url:'http://127.0.0.1:8000/api/task', data:postdata, type:'POST'
-//     }).done(function(){})
+//   $.ajax({
+//     method: 'POST',
+//     url: 'http://127.0.0.1:8000/api/task/',
+//     // csrfmiddlewaretoken: '{{ csrf_token }}',
+//     username: '',
+//     password: '',
+//     data: {
+//       csrfmiddlewaretoken: '{{ csrf_token }}',
+//       title: title.val(),
+//       status: status.val(),
+//       priority: priority.val(),
+//     }
+//   });
+//
+// //   return false;
 // }
-//
-//
-// function pickTask(){
-//     specific_task = "http://127.0.0.1:8000/api" + str(selection)
-//     $.ajax({url: "http://127.0.0.1:8000/api", type: 'GET'}).done(function(results){
-//         var tasks = results.results
-// }
-//
-//
-//
+
+
+function taskPost(){
+    console.log('you added something new')
+  var postdata = {'title': 'its over', 'status': 'c', 'priority': 'l', 'description': 'these are words'}
+  jQuery.ajax({url:'http://127.0.0.1:8000/api/task/', data:postdata, datatype: 'jsonp', type:'POST'
+    }).done(function(results){
+    })
+}
+
+
+// Delete
 // function removeTask(selection){
-//     specific_task = "http://127.0.0.1:8000/api" + str(selection)
+//     specific_task = "http://127.0.0.1:8000/api/task/" + "1 "
 //     $.ajax({url: specific_task, type: 'DELETE'}).done(function(results){
-//         var tasks = results.results
-// }
-//
-//
-// function putTask(selection){
-//     specific_task = "http://127.0.0.1:8000/api" + str(selection)
-//     $.ajax({url: specific_task, type: 'PUT'}).done(function(results){
-//         var tasks = results.results
+//         $('#removetasks').html(results['title'])
+//         // var tasks = results.results
+//     })
 // }
 
 
+//GET//
 function taskList(){
+    console.log("getting stuff")
     var $orderedlist = $("<ol>")
-        $.ajax("http://127.0.0.1:8000/api/task").done(function(results){
+        $.ajax("http://127.0.0.1:8000/api/task/").done(function(results){
             var tasks = results.results
             for(var i = 0; i < tasks.length; i++){
                 $orderedlist.html($orderedlist.html()+ tasks[i]['title'] + ": "),
@@ -41,4 +89,9 @@ function taskList(){
             }
         })
 }
-taskList()
+
+
+// $("#postButton").click(taskPost)
+$("#getButton").click(taskList)
+// $("#deletebutton").click(removeTask)
+$("#try_post").click(taskPost)
